822667a87f9e0cb1b1ac51b681d5f4c2908dccf6
krsazon <krsazon@yahoo.com>
2016-07-05 11:58:30 +0800

    Commitnanamin
    ayosnato

--- Hotel/Shared/Page/LoginPage.xaml.cs
+++ Hotel/Shared/Page/LoginPage.xaml.cs
@@ -32,24 +32,24 @@ namespace Hotel.Page
 
        public void btnOK_Click(object sender, RoutedEventArgs e)
         {
-             //using (var context = new DatabaseContext())
-             //{
-             //  var user = context.Users.Where(c => c.Username == this.txtUsername.Text.ToLower() && c.Password == this.txtPassword.Text).SingleOrDefault();
-             //  if (user != null)
-             //  {
-             //      var users = context.Users.FirstOrDefault(c => c.Username == txtUsername.Text);
-                 MethodsClass.ShowNotification("ERROR KAY SWEET");
-                 //var frame = DevExpress.Xpf.Core.Native.LayoutHelper.FindParentObject<NavigationFrame>(this);
-                 //MainViewPage page = new MainViewPage();
-                 //frame.Navigate(page);
-               //  tx = users.Name;
-               //  ty = users.UserType;
-               //}
-               //else
-               //{
-               //MethodsClass.ShowNotification("Logged in failed.");
-               //}
-            //}
+            using (var context = new DatabaseContext())
+            {
+                var user = context.Users.Where(c => c.Username == this.txtUsername.Text.ToLower() && c.Password == this.txtPassword.Text).SingleOrDefault();
+                if (user != null)
+                {
+                    var users = context.Users.FirstOrDefault(c => c.Username == txtUsername.Text);
+
+                    var frame = DevExpress.Xpf.Core.Native.LayoutHelper.FindParentObject<NavigationFrame>(this);
+                    MainViewPage page = new MainViewPage();
+                    frame.Navigate(page);
+                    tx = users.Name;
+                    ty = users.UserType;
+                }
+                else
+                {
+                    MethodsClass.ShowNotification("Logged in failed.");
+                }
+            }
         }
 
         private void btnExit_Click(object sender, RoutedEventArgs e)
